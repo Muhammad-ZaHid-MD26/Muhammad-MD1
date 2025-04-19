ff0a0905866327dc1dffe82add84a17b99e920e9
Muhammad-ZaHid-MD26 <muhammadzahid132181131314@gmail.com>
2025-04-19 16:23:10 +0500

    main-list.js

--- plugins/main-list.js
+++ plugins/main-list.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         : m.sender
   if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
 
-  let pp = './assets/A.jpg'
+  let pp = './assets/Aa.jpg'
   let more = String.fromCharCode(8206)
   let readMore = more.repeat(850)
 
