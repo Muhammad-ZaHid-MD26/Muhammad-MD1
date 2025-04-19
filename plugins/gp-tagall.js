7e6f1329873a37684f1fb138ddb910d234c49261
Muhammad-ZaHid-MD26 <muhammadzahid132181131314@gmail.com>
2025-04-19 14:10:49 +0500

    gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
   m.reply(
     `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
       users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n└──✪ Ultra ┃ ᴮᴼᵀ ✪──',
+      '\n`𝙈𝙪𝙝𝙖𝙢𝙢𝙖𝙙-𝙈𝘿`',
     null,
     {
       mentions: users,
